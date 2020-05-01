623bc83cd52cca5c28e8e2fa6466898fd31e4a74
Jonathan Grahl <nikentic@gmail.com>
2020-05-01 16:30:03 +0200

    Fix linting

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -68,9 +68,14 @@ class CrispyFilterTests(SimpleTestCase):
             terms of service<span class="asteriskField">*</span> </label>
             <div class=" mb-3">
             <div class="relative">
-            <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" name="tos_accepted">            <option value="accepted">Accepted</option>            <option value="not_accepted">Not accepted</option>            </select>            <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"></path> </svg> </div>
-            </div> </div>
-            </div>
+            <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8
+            rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" name="tos_accepted">
+            <option value="accepted">Accepted</option>
+            <option value="not_accepted">Not accepted</option>
+            </select><div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+            <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"></path> 
+            </svg> 
+            </div> </div></div>
             """,
         )
