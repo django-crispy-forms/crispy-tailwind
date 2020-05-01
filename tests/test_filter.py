8a4bab1048fa8c9e0c8d348d2a420df469379881
Jonathan Grahl <nikentic@gmail.com>
2020-05-01 15:20:22 +0200

    Templating and styling for Select widget

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -5,6 +5,8 @@ from .forms import SampleForm
 
 
 class CrispyFilterTests(SimpleTestCase):
+    maxDiff = None
+
     def test_crispy_filter(self):
         template = Template(
             """
@@ -15,6 +17,7 @@ class CrispyFilterTests(SimpleTestCase):
         form = SampleForm()
         c = Context({"form": form})
         html = template.render(c)
+
         self.assertHTMLEqual(
             html,
             """<div id="div_id_is_company" class=" mb-3">
@@ -58,6 +61,16 @@ class CrispyFilterTests(SimpleTestCase):
             date time<span class="asteriskField">*</span> </label>
             <input type="text" name="datetime_field_0" class="dateinput " required id="id_datetime_field_0">
             <input type="text" name="datetime_field_1" class="timeinput " required id="id_datetime_field_1">
+            </div>
+
+            <div id="div_id_tos_accepted" class=" mb-3">
+            <label for="id_tos_accepted" class="block text-gray-700 text-sm font-bold mb-2">
+            terms of service<span class="asteriskField">*</span> </label>
+            <div class=" mb-3">
+            <div class="relative">
+            <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" name="tos_accepted">            <option value="accepted">Accepted</option>            <option value="not_accepted">Not accepted</option>            </select>            <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"></path> </svg> </div>
+            </div> </div>
             </div>
             """,
         )
