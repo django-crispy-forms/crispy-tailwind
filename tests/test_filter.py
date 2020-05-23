3b5db6e424b0bb0e0ca1f95728b6a147ede95faf
David Smith <39445562+smithdc1@users.noreply.github.com>
2020-05-23 21:39:19 +0100

    Updated select css  (#15)
    * Select input now has same default `class` as other inputs

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -83,8 +83,8 @@ class CrispyFilterTests(SimpleTestCase):
             terms of service<span class="asteriskField">*</span> </label>
             <div class=" mb-3">
             <div class="relative">
-            <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 """
-            """pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" name="tos_accepted">
+            <select class="bg-white focus:outline-none border border-gray-300 rounded-lg py-2 px-4 block w-full """
+            """appearance-none leading-normal text-gray-700" name="tos_accepted">
             <option value="accepted">Accepted</option>
             <option value="not_accepted">Not accepted</option>
             </select><div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
