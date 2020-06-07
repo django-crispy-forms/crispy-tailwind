a5bcc660fdf2cfb1987d1eb6e07375816d907f25
David Smith <smithdc@gmail.com>
2020-06-07 09:36:55 +0100

    Field and Label class defaults
    These are added automatically when using crispy filter but need
    to add them for crispy tag. Added them to the template for now
    could revisit the crispy-filter method.

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -24,7 +24,7 @@ class CrispyHelperTests(SimpleTestCase):
         html = render_crispy_form(form)
         expected_html = """
         <form  method="post" > 
-        <div id="div_id_name" class=""> <label for="id_name" class="">
+        <div id="div_id_name" class="mb-3"> <label for="id_name" class="block font-bold mb-2 text-gray-700 text-sm">
             Name<span class="asteriskField">*</span> 
             </label> 
             <input type="text" name="name" class="textinput textInput inputtext block bg-white rounded-lg leading-normal border-gray-300 px-4 appearance-none border focus:outline-none w-full py-2 text-gray-700" required id="id_name"> 
@@ -38,8 +38,8 @@ class CrispyHelperTests(SimpleTestCase):
         html = render_crispy_form(form)
         expected_html = """
         <form  method="post" > 
-        <div id="div_id_password" class=""> 
-        <label for="id_password" class="">
+        <div id="div_id_password" class="mb-3"> 
+        <label for="id_password" class="block font-bold mb-2 text-gray-700 text-sm">
             Password<span class="asteriskField">*</span> </label> 
             <input type="password" name="password" class="passwordinput bg-white text-gray-700 w-full px-4 border py-2 border-gray-300 block rounded-lg focus:outline-none leading-normal appearance-none" required id="id_password"> 
             </div> </form>"""
@@ -50,8 +50,8 @@ class CrispyHelperTests(SimpleTestCase):
         form.helper = FormHelper()
         html = render_crispy_form(form)
         expected_html = """
-        <form  method="post" > <div id="div_id_radio" class=""> <label for="id_radio_0" class="">
-            Radio<span class="asteriskField">*</span> </label> <div class=""> 
+        <form  method="post" > <div id="div_id_radio" class="mb-3"> <label for="id_radio_0" class="block font-bold mb-2 text-gray-700 text-sm">
+            Radio<span class="asteriskField">*</span> </label> <div class="mb-3"> 
             <label for="id_radio_1" class="block text-gray-700 mr-3"> <input type="radio" class="" name="radio" id="id_radio_1" value="blue" >
         Blue
     </label> <label for="id_radio_2" class="block text-gray-700 mr-3"> <input type="radio" class="" name="radio" id="id_radio_2" value="green" >
@@ -65,8 +65,8 @@ class CrispyHelperTests(SimpleTestCase):
         form.helper = FormHelper()
         html = render_crispy_form(form)
         expected_html = """
-<form  method="post" > <div id="div_id_radio" class=""> <label for="" class="">
-            Radio<span class="asteriskField">*</span> </label> <div class=""> <div class="mr-3"> <label class="block text-gray-700" for="id_radio_1"> <input type="checkbox" class="" name="radio" id="id_radio_1" value="blue" >
+<form  method="post" > <div id="div_id_radio" class="mb-3"> <label for="" class="block font-bold mb-2 text-gray-700 text-sm">
+            Radio<span class="asteriskField">*</span> </label> <div class="mb-3"> <div class="mr-3"> <label class="block text-gray-700" for="id_radio_1"> <input type="checkbox" class="" name="radio" id="id_radio_1" value="blue" >
             Blue
         </label> </div> <div class="mr-3"> <label class="block text-gray-700" for="id_radio_2"> <input type="checkbox" class="" name="radio" id="id_radio_2" value="green" >
             Green
@@ -82,9 +82,9 @@ class CrispyHelperTests(SimpleTestCase):
         form.helper.layout = Layout("is_company", "first_name")
         html = render_crispy_form(form)
         expected_html = """
-<form  method="post" > <div id="div_id_is_company" class=""> <label for="id_is_company" class="">
+<form  method="post" > <div id="div_id_is_company" class="mb-3"> <label for="id_is_company" class="block font-bold mb-2 text-gray-700 text-sm">
             company
-        </label> <input type="checkbox" name="is_company" class="checkboxinput " id="id_is_company"> </div> <div id="div_id_first_name" class=""> <label for="id_first_name" class="">
+        </label> <input type="checkbox" name="is_company" class="checkboxinput " id="id_is_company"> </div> <div id="div_id_first_name" class="mb-3"> <label for="id_first_name" class="block font-bold mb-2 text-gray-700 text-sm">
             first name<span class="asteriskField">*</span> </label> <input type="text" name="first_name" maxlength="5" class="textinput textInput inputtext rounded-lg leading-normal border text-gray-700 w-full block border-gray-300 appearance-none bg-white focus:outline-none px-4 py-2" required id="id_first_name"> </div> </form>
             """
         self.assertHTMLEqual(html, expected_html)
