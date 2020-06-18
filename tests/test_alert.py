1011ad12c5ec21e6cbf1b21e6b96f237b8a9d498
David Smith <smithdc@gmail.com>
2020-06-18 22:32:01 +0100

    space in test

--- tests/test_alert.py
+++ tests/test_alert.py
@@ -49,7 +49,7 @@ class CrispyHelperTests(SimpleTestCase):
         )
         html = render_crispy_form(form)
         expected_html = """
-            <div class="custom css" role="alert">
+            <div class=" custom css" role="alert">
                 <div class="block sm:inline"><strong>Warning!</strong> Here's a test message.</div>
                 <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
                     <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
