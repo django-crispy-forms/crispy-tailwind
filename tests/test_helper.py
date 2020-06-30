585c1f68288848e71addec8aab13bf311d2c5797
David Smith <smithdc@gmail.com>
2020-06-30 07:30:19 +0100

    Added top border on formset errors_formset.html
    * If there is no heading then a top border is needed.
    * When there is an alert, colour is similar so it blends ok.

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -302,7 +302,7 @@ class CrispyHelperTests(SimpleTestCase):
                 <div class="bg-red-500 text-white font-bold rounded-t px-4 py-2">
                     Non Form Errors
                 </div>
-                <div class="border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700">
+                <div class="border border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700">
                     <ul>
                         <li>Please submit 2 or fewer forms.</li>
                     </ul>
