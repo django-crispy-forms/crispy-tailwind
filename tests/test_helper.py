d6bcba0b9e00648041b109d59ee616d6d552fa9c
Sab <shabi@fossix.org>
2020-07-08 22:57:10 +0200

    set selected for option with field value (#52)
    * set selected for option with field value
    * Added Select Test

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -8,7 +8,15 @@ from crispy_forms.layout import HTML, Column, Div, Field, Fieldset, Hidden, Layo
 from crispy_forms.utils import render_crispy_form
 from crispy_tailwind.layout import Button, Reset, Submit
 
-from .forms import CharFieldForm, CheckboxMultiple, PasswordFieldForm, RadioForm, SampleForm, ShortCharFieldForm
+from .forms import (
+    CharFieldForm,
+    CheckboxMultiple,
+    PasswordFieldForm,
+    RadioForm,
+    SampleForm,
+    SelectForm,
+    ShortCharFieldForm,
+)
 
 template = Template(
     """
@@ -521,6 +529,29 @@ class CrispyHelperTests(SimpleTestCase):
                     id="id_password2"
                 />
                 <p id="error_1_id_password2" class="text-red-500 text-xs italic"><strong>This field is required.</strong></p>
+            </div>
+        </div>
+        """
+        self.assertHTMLEqual(html, expected_html)
+
+    def test_select(self):
+        form = SelectForm(data={"tos_accepted": "accepted"})
+        form.helper = FormHelper()
+        form.helper.form_tag = False
+        html = render_crispy_form(form)
+        expected_html = """
+        <div id="div_id_tos_accepted" class="mb-3">
+            <label for="id_tos_accepted" class="block text-gray-700 text-sm font-bold mb-2"> terms of service<span class="asteriskField">*</span> </label>
+            <div class="mb-3">
+                <div class="relative">
+                    <select class="bg-white focus:outline-none border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal text-gray-700" name="tos_accepted">
+                        <option value="accepted" selected>Accepted</option>
+                        <option value="not_accepted">Not accepted</option>
+                    </select>
+                    <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
+                    </div>
+                </div>
             </div>
         </div>
         """
