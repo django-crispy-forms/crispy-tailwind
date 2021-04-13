d16838dc309f9d8cb9bc6080ba460f010809ec2d
David Smith <smithdc@gmail.com>
2021-04-13 08:22:24 +0100

    Version 0.5

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2020, David Smith"
 author = "David Smith"
 
 # The full version, including alpha/beta/rc tags
-release = "0.4"
+release = "0.5"
 
 
 # -- General configuration ---------------------------------------------------
